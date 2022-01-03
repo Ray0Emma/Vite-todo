de228384fd6ff7c1b5ed49fe65ef9faf8c89c82a
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-03 15:55:00 +0000

    feat: added todo component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,83 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+<h1>{{ msg }}</h1>
+    <form @submit.prevent="addTodo()">
+        <label>New ToDo </label>
+        <input
+            v-model="newTodo"
+            name="newTodo"
+            autocomplete="off"
+        >
+        <button>Add ToDo</button>
+    </form>
+    <h2>ToDo List</h2>
+    <ul v-if="todos.length > 0">
+        <li
+            v-for="(todo, index) in todos"
+            :key="index"
+        >
+            <span
+                :class="{ done: todo.done }"
+                @click="doneTodo(todo)"
+            >{{ todo.content }}</span>
+            <button @click="removeTodo(index)">Remove</button>
+        </li>
+    </ul>
+    <h4 v-else>Empty list.</h4>
 </template>
 
+<script>
+import { ref } from "vue";
+
+export default {
+  // name: 'Todo',
+  setup() {
+    const newTodo  = ref("");
+    const defaultData = [{
+                done: false,
+                content: 'Write a blog post'
+            }]
+            const todosData = JSON.parse(localStorage.getItem('todos')) || defaultData;
+            const todos = ref(todosData);
+            function addTodo () {
+                if (newTodo.value) {
+                    todos.value.push({
+                        done: false,
+                        content: newTodo.value
+                    });
+                    newTodo.value = '';
+                }
+                saveData();
+            }
+            function doneTodo (todo) {
+                todo.done = !todo.done
+                saveData();
+            }
+            function removeTodo (index) {
+                todos.value.splice(index, 1);
+                saveData();
+            }
+            function saveData () {
+                const storageData = JSON.stringify(todos.value);
+                localStorage.setItem('todos', storageData);
+            }
+    return {
+      todos,
+                newTodo,
+                addTodo,
+                doneTodo,
+                removeTodo,
+                saveData
+    };
+  },
+  props: {
+    msg: String,
+  },
+}
+
+</script>
+
 <style scoped>
-a {
-  color: #42b983;
+.done {
+  text-decoration: line-through;
 }
 </style>
