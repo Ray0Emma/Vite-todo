eee79a7c6be6000d322307a5439a0dcb6d18022c
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-04 16:59:54 +0000

    feat: added date and tasks number

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,22 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
-  }
-}
+  },
+};
 </script>
 <style>
+body {
+  background-color: #9ca3af;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,50 @@
 <template>
-<h1>{{ msg }}</h1>
-    <form @submit.prevent="addTodo()">
-        <label>New ToDo </label>
+  <!-- <h1>{{ msg }}</h1> -->
+  <div class="container-md">
+    <div class="p-6 max-w-md mx-auto bg-gray-200 rounded-xl shadow-lg">
+      <h1 class="text-lg font-semibold mb-3">
+        {{
+          new Date().toLocaleString("en-us", {
+            weekday: "long",
+            day: "numeric",
+            month: "short",
+          })
+        }}
+      </h1>
+      <span class="text-cyan-600 font-base">{{ todos.length }} tasks</span>
+      <form @submit.prevent="addTodo()" class="mt-7 mb-7 flex justify-between">
         <input
-            v-model="newTodo"
-            name="newTodo"
-            autocomplete="off"
-        >
-        <button>Add ToDo</button>
-    </form>
-    <h2>ToDo List</h2>
-    <ul v-if="todos.length > 0">
+          class="border-2 border-cyan-600 outline-0 bg-slate-200"
+          v-model="newTodo"
+          name="newTodo"
+          autocomplete="off"
+        />
+        <button class="ml-3">Add</button>
+      </form>
+      <ul v-if="todos.length > 0">
         <li
-            v-for="(todo, index) in todos"
-            :key="index"
+          v-for="(todo, index) in todos"
+          :key="index"
+          class="flex justify-between"
         >
-            <span
-                :class="{ done: todo.done }"
-                @click="doneTodo(todo)"
-            >{{ todo.content }}</span>
-            <button @click="removeTodo(index)">Remove</button>
+          <div class="inline">
+            <input
+              class="mr-2"
+              type="checkbox"
+              :name="todo.content"
+              :checked="todo.done"
+              @click="doneTodo(todo)"
+            />
+            <label :for="todo.content" :class="{ done: todo.done }">{{
+              todo.content
+            }}</label>
+          </div>
+          <button @click="removeTodo(index)" class="text-end">Remove</button>
         </li>
-    </ul>
-    <h4 v-else>Empty list.</h4>
+      </ul>
+      <h4 v-else class="text-center">Empty list.</h4>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -31,49 +53,50 @@ import { ref } from "vue";
 export default {
   // name: 'Todo',
   setup() {
-    const newTodo  = ref("");
-    const defaultData = [{
-                done: false,
-                content: 'Write a blog post'
-            }]
-            const todosData = JSON.parse(localStorage.getItem('todos')) || defaultData;
-            const todos = ref(todosData);
-            function addTodo () {
-                if (newTodo.value) {
-                    todos.value.push({
-                        done: false,
-                        content: newTodo.value
-                    });
-                    newTodo.value = '';
-                }
-                saveData();
-            }
-            function doneTodo (todo) {
-                todo.done = !todo.done
-                saveData();
-            }
-            function removeTodo (index) {
-                todos.value.splice(index, 1);
-                saveData();
-            }
-            function saveData () {
-                const storageData = JSON.stringify(todos.value);
-                localStorage.setItem('todos', storageData);
-            }
+    const newTodo = ref("");
+    const defaultData = [
+      {
+        done: false,
+        content: "Write a blog post",
+      },
+    ];
+    const todosData = JSON.parse(localStorage.getItem("todos")) || defaultData;
+    const todos = ref(todosData);
+    function addTodo() {
+      if (newTodo.value) {
+        todos.value.push({
+          done: false,
+          content: newTodo.value,
+        });
+        newTodo.value = "";
+      }
+      saveData();
+    }
+    function doneTodo(todo) {
+      todo.done = !todo.done;
+      saveData();
+    }
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+      saveData();
+    }
+    function saveData() {
+      const storageData = JSON.stringify(todos.value);
+      localStorage.setItem("todos", storageData);
+    }
     return {
       todos,
-                newTodo,
-                addTodo,
-                doneTodo,
-                removeTodo,
-                saveData
+      newTodo,
+      addTodo,
+      doneTodo,
+      removeTodo,
+      saveData,
     };
   },
   props: {
     msg: String,
   },
-}
-
+};
 </script>
 
 <style scoped>
